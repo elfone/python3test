afa0632b3360c0150a17eabeab1faf60d7d18706
elfone <elfone@yeah.net>
2015-03-20 11:34:47 +0800

    1-2显示系统信息 uname -a df -h

---
+++ python unix和linux系统管理/1-2显示系统信息.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# A system Information Gathering SCript
+import subprocess
+
+# Command 1
+uname = "uname"
+uname_arg = "-a"
+print("Gathering system information with {} command:\n".format(uname))
+subprocess.call([uname, uname_arg])
+
+# Command 2
+diskspcae = "df"
+diskspcae_arg = '-h'
+print("Gathering diskspace information {} command:\n".format(diskspcae))
+subprocess.call([diskspcae, diskspcae_arg])
+
+
